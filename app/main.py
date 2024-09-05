2bc556f5b14e0217aa3dfaa6243b2c2d4c325bf9
p1tonidze <p1t0nidze422@gmail.com>
2024-09-05 20:23:37 +0300

    Solution py-vector-class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,73 @@
+import math
+from typing import Union, Tuple
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
+        self.x = round(x_coordinate, 2)
+        self.y = round(y_coordinate, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        if isinstance(other, Vector):
+            new_x = self.x + other.x
+            new_y = self.y + other.y
+            return Vector(new_x, new_y)
+        return NotImplemented
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        if isinstance(other, Vector):
+            new_x = self.x - other.x
+            new_y = self.y - other.y
+            return Vector(new_x, new_y)
+        return NotImplemented
+
+    def __mul__(self, other: Union["Vector", int, float]) ->\
+            Union[float, "Vector"]:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        elif isinstance(other, (int, float)):
+            new_x = self.x * other
+            new_y = self.y * other
+            return Vector(new_x, new_y)
+        return NotImplemented
+
+    @classmethod
+    def create_vector_by_two_points(
+        cls, start_point: Tuple[float, float], end_point: Tuple[float, float]
+    ) -> "Vector":
+        delta_x = round(end_point[0] - start_point[0], 2)
+        delta_y = round(end_point[1] - start_point[1], 2)
+        return cls(delta_x, delta_y)
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        normalized_x = self.x / length
+        normalized_y = self.y / length
+        return Vector(normalized_x, normalized_y)
+
+    def angle_between(self, other: "Vector") -> float:
+        if not isinstance(other, Vector):
+            return NotImplemented
+        dot_product = self * other
+        length_self = self.get_length()
+        length_other = other.get_length()
+        if length_self == 0 or length_other == 0:
+            return 0.0  # Avoid division by zero
+        cos_angle = dot_product / (length_self * length_other)
+        cos_angle = max(-1.0, min(1.0, cos_angle))  # Clamp value to [-1, 1]
+        angle = math.degrees(math.acos(cos_angle))
+        return round(angle)
+
+    def get_angle(self) -> float:
+        return round(math.degrees(math.atan2(self.x, self.y))) * -1
+
+    def rotate(self, degrees: float) -> "Vector":
+        radians = math.radians(degrees)
+        rotated_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        rotated_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(round(rotated_x, 2), round(rotated_y, 2))
